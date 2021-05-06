8554a3478661f2798d167ee2ea812e850241b328
Juan Pablo <jp.cano@uniandes.edu.co>
2021-05-06 13:08:13 -0500

    Almost Done

--- script.py
+++ script.py
@@ -1,8 +1,13 @@
+import httpx
 import httpx as http
 from dotenv import load_dotenv, find_dotenv
 import os
 import pandas as pd
 import numpy as np
+import time
+import hashlib
+import sqlite3
+from tqdm.auto import tqdm
 
 load_dotenv(find_dotenv())
 
@@ -14,28 +19,102 @@ REGIONS_BASE_HEADERS = {
 
 COUNTRY_BASE_URL = "https://restcountries.eu/rest/v2/region/"
 
+class Challenge:
+    def __init__(self):
+        self.data = self.init_data()
+        self.conn = sqlite3.connect(os.getenv("SQLITE_DB", "example.db"))
+        self.cursor = self.conn.cursor()
+        self.init_db()
 
-def get_regions():
-    """
-    :return:
-    :rtype:
-    """
-    response = http.get(REGIONS_BASE_URL + "all", headers=REGIONS_BASE_HEADERS)
-    response = response.json()
-    data = {
-        "Region": [],
-        "City Name": [],
-        "Language": [],
-        "Time": []
-    }
-    for country in response:
-        if country["region"]:
-            data["Region"].append(country["region"])
+    def init_db(self):
+        try:
+            self.cursor.execute("""CREATE TABLE countries(
+                id INTEGER PRIMARY KEY,
+                region TEXT,
+                city_name TEXT,
+                language TEXT,
+                time FLOAT
+            )""")
+        except Exception as e:
+            print(str(e))
+
+    @staticmethod
+    def get_regions():
+        response = httpx.get(
+            REGIONS_BASE_URL + "all",
+            headers=REGIONS_BASE_HEADERS
+        ).json()
+        regions = {}
+        for country in response:
+            if country["region"]:
+                regions[country["region"]] = 1
+        return list(regions.keys())
+
+    def init_data(self):
+        data = {
+            "Region": [],
+            "City Name": [],
+            "Language": [],
+            "Time": []
+        }
+        regions = self.get_regions()
+        for region in tqdm(regions):
+            start_time = time.time()
+            data["Region"].append(region)
             response = http.get(
-                COUNTRY_BASE_URL + country["region"].lower()
+                COUNTRY_BASE_URL + region.lower()
             ).json()
+            random_country_index = np.random.randint(0, len(response))
+            country = response[random_country_index]
+            data["City Name"].append(country["name"])
+            random_language_index = np.random.randint(0, len(country["languages"]))
+            language = country["languages"][random_language_index]
+            data["Language"].append(
+                hashlib.sha1(
+                    language["name"].encode("utf-8")
+                ).hexdigest()
+            )
+            end_time = time.time()
+            data["Time"].append(np.round(end_time - start_time, 2))
+        return pd.DataFrame(data)
 
-    return
+    def time_statistics(self):
+        return self.data.agg({
+            "Time": ["sum", "mean", "min", "max"]
+        })
+
+    def insert_data(self):
+        for index, row in self.data.iterrows():
+            try:
+                self.cursor.execute(f"""INSERT INTO countries(
+                    id,  region, city_name, 
+                    language, time
+                ) VALUES (
+                    {index},
+                    '{row["Region"]}',
+                    '{row["City Name"]}',
+                    '{row["Language"]}',
+                    {row["Time"]}
+                )""")
+            except Exception as e:
+                print(str(e))
+        self.conn.commit()
+        return
+
+    def save_json(self):
+        self.data.to_json("data.json", orient="split", index=False)
+
+    def finish(self):
+        self.cursor.execute("DROP TABLE countries")
+        self.conn.commit()
 
 
-print(get_regions())
+challenge = Challenge()
+print("Initiated")
+print(challenge.time_statistics())
+challenge.insert_data()
+print("Data inserted")
+challenge.finish()
+print("Table deleted")
+challenge.save_json()
+print("JSON Generated")
